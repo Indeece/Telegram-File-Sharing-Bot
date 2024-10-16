ba15199b4a26c5465857041641853023f623e9e9
Indeece <anton.shkonda@yandex.ru>
2024-10-16 18:07:12 +0300

    Added menu

--- dispatcher/src/main/java/ru/relex/controller/TelegramBot.java
+++ dispatcher/src/main/java/ru/relex/controller/TelegramBot.java
@@ -4,10 +4,17 @@ import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScope;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
 
 @Log4j
 @Component
@@ -26,6 +33,18 @@ public class TelegramBot extends TelegramLongPollingBot {
     @PostConstruct
     public void init() {
         updateController.registerBot(this);
+        // Переносим установку команд в метод init
+        List<BotCommand> listOfCommands = new ArrayList<>();
+        listOfCommands.add(new BotCommand("/registration", "Регистрация пользователя"));
+        listOfCommands.add(new BotCommand("/cancel", "Отмена загрузки контента"));
+        listOfCommands.add(new BotCommand("/about", "Информация о создателе бота"));
+        listOfCommands.add(new BotCommand("/report", "Сообщить об ошибке"));
+
+        try {
+            this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e) {
+            log.error("Ошибка при установке команд: " + e.getMessage());
+        }
     }
 
     @Override
